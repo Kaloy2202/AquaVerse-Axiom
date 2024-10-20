6ffd280f3dcb154f3c8a76b0397d3f8dd98be4d1
Kyle Neo <95029881+Kaloy2202@users.noreply.github.com>
2024-10-20 14:08:30 +0800

    added sounds
    - ambient sound
    - background music for daytime
    - sfx for walk

---
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    // This method will automatically play background music when the game starts
+    void Start()
+    {
+        Play("AmbientSound");//ith the actual name of your background music
+    }
+
+    // Play the sound by name
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    // Stop the sound by name
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+}
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 0.7f;
+
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}

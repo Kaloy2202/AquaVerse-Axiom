c04725bb239886a4e940353fb2449c03b8fc4527
Kristan Jay Gabay <ktgabay@up.edu.ph>
2024-10-25 22:45:50 +0800

    keybinding

--- Assets/Scripts/CanvasSwitcher.cs
+++ Assets/Scripts/CanvasSwitcher.cs
@@ -5,9 +5,10 @@ public class CanvasSwitcher : MonoBehaviour
 {
     public Canvas inGameUI;
     public Canvas miniGameUI;
-    public Canvas fishDemandMarketUI;
+    public Image fishDemandMarketUI;
     public Button miniGameButton;
-    public AudioManager audioManager; // Reference to AudioManager
+    public GameObject BuyerManager;
+    private bool isMarketOpen = false;
 
     private void Start()
     {
@@ -40,9 +41,11 @@ public class CanvasSwitcher : MonoBehaviour
         {
             SwitchCanvas(miniGameUI);
         }
-        if (Input.GetKeyDown(KeyCode.F2))
+        if (Input.GetKeyDown(KeyCode.F2) && PlayerStats.Instance.level >= 3)
         {
-            SwitchCanvas(fishDemandMarketUI);
+            isMarketOpen = !isMarketOpen;
+            fishDemandMarketUI.gameObject.SetActive(isMarketOpen);
+            BuyerManager.SetActive(isMarketOpen);
         }
     }
 
